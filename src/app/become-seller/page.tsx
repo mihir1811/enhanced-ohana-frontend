2a005cb584fd77b3307ec0e9e571f981dea1d5a5
Mihir Sabhaya <mihir.roxontechnologies@gmail.com>
2025-09-01 15:19:12 +0530

    format code

--- src/app/become-seller/page.tsx
+++ src/app/become-seller/page.tsx
@@ -4,12 +4,12 @@ import Footer from '@/components/Footer';
 
 export default function Page() {
 	return (
-        <>
-            <NavigationUser />
-		<div className="min-h-screen bg-gray-50 py-10">
-			<BecomeSellerForm />
-		</div>
-            <Footer />
-        </>
+		<>
+			<NavigationUser />
+			<div className="min-h-screen bg-gray-50 py-10">
+				<BecomeSellerForm />
+			</div>
+			<Footer />
+		</>
 	);
 }
