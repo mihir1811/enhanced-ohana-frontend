832e58057d813ff4ea00e72deb99f6d08f84db16
Mihir Sabhaya <mihir.roxontechnologies@gmail.com>
2025-11-05 15:34:38 +0530

    fis build issue

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,12 @@ const nextConfig: NextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "images.stockcake.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
   typescript: {
